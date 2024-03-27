6e67eafb97d64c9219dc1232a6169451e2fe08c5
Valerie <valerie.ssintasath@gmail.com>
2024-03-27 17:00:06 +0100

    axios et useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import "./App.css";
+import axios from "axios";
+
+import { useEffect } from "react";
 
 // Je renomme BrowserRouter en Router
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -10,9 +13,23 @@ import Offer from "./pages/Offer";
 //import des composants
 import Header from "./components/Header";
 
+//import images
 import vintedLogo from "./assets/imgs/vinted-logo.svg";
 
 function App() {
+  //utiliser useEffect()
+  //faire une requete au back en creant une fonction fetchData() afin d'utiliser le async
+  //ne pas oublier d'appeler la fonction après l'avoir défini!!
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(
+        "https://lereacteur-vinted-api.herokuapp.com/offers"
+      );
+      console.log(response.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     // Router doit contenir tout mon site
     <Router>
